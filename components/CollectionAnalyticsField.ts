3095cffcf7ab3724dae59ea2f333c558269237f2
nick <nick0vogel@gmail.com>
2025-07-13 10:58:48 -0400

    fix: correct CollectionAnalyticsField export for Payload dynamic imports

--- components/CollectionAnalyticsField.ts
+++ components/CollectionAnalyticsField.ts
@@ -1,3 +1,1 @@
-// This file is used for dynamic imports in Payload CMS
-// It re-exports the component from the src directory
-export { default } from '../src/components/CollectionAnalyticsField'
+export { CollectionAnalyticsField as default } from '../dist/components/CollectionAnalyticsField'
